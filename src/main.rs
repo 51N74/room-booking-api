fadee9e4878161cbfb366693333684417cacadad
51N72 <j.pattanasintara@gmail.com>
2025-05-16 10:23:12 +0700

    create-config

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+use room_booking_api::config::config_loader;
+use tracing::info;
+
+#[tokio::main]
+async fn main() {
+    tracing_subscriber::fmt()
+        .with_max_level(tracing::Level::DEBUG)
+        .init();
+
+    let dotenvy_env = match config_loader::load(){
+        Ok(env)=>env,
+        Err(e)=>{
+            tracing::error!("Failed to load environment variables: {}", e);
+            return;
+        }
+    };
+
+    info!("Env has been loaded {:?}", dotenvy_env);
+
+
 }
